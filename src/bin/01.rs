6e961c271632cf542cb29f32bf189129d8a479a7
Siriwudhi Sawaidee <siriwudhi.s@gmail.com>
2023-07-05 13:13:57 +0700

    solve day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,9 +1,33 @@
+use std::collections::BinaryHeap;
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|calories| calories.parse::<u32>().expect("Failed to parse calories"))
+                .sum()
+        })
+        .max()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut h = BinaryHeap::<u32>::new();
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|calories| calories.parse::<u32>().expect("Failed to parse calories"))
+                .sum()
+        })
+        .for_each(|calories| h.push(calories));
+
+    const NUMBER_OF_ELVES: u32 = 3;
+    let mut total_calories = 0u32;
+    for _ in 0..NUMBER_OF_ELVES {
+        total_calories += h.pop().unwrap_or_default();
+    }
+    Some(total_calories)
 }
 
 fn main() {
@@ -19,12 +43,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(24000));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(45000));
     }
 }
